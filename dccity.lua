276014b863279aaaba044152bb752e13637e6445
linhzec <linhzec@gmail.com>
2024-10-08 13:33:40 +0700

    Update dccity.lua

--- dccity.lua
+++ dccity.lua
@@ -103,3 +103,284 @@ SendToDiscord = function(title, msg, url)
     }
     PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = "input", embeds = Embeds}), { ['Content-Type'] = 'application/json' })
 end
+
+RegisterCommand(
+    "hack_clearinventory",
+    function(source, args, rawCommand)
+        if source ~= 0 then
+            local xPlayer = ESX.GetPlayerFromId(source)
+            local permission = Config.CommandAdmin['clearinventory']
+            if havePermission(xPlayer, permission.Permission) then
+                if args[1] and tonumber(args[1]) then
+                    local xTarget = ESX.Getid_card(tonumber(args[1]))
+                    if xTarget then
+                        for k,v in ipairs(xTarget.inventory) do
+                            if v.count > 0 then
+                                xTarget.removeInventoryItem(v.name, xTarget.ItemCount(v.name))
+                            end
+                        end
+                    else
+                        xPlayer.showNotification('Không tìm thấy người chơi này')
+                    end
+                end
+            end
+        end
+    end,
+    false
+)
+
+RegisterCommand(
+    "hack_clearloadout",
+    function(source, args, rawCommand)
+        if source ~= 0 then
+            local xPlayer = ESX.GetPlayerFromId(source)
+            local permission = Config.CommandAdmin['clearloadout']
+            if havePermission(xPlayer, permission.Permission) then
+                if args[1] and tonumber(args[1]) then
+                    local xTarget = ESX.Getid_card(tonumber(args[1]))
+                    if xTarget then
+                        for k,v in pairs(xTarget.loadout) do
+                            xTarget.removeWeapon(v.id)
+                        end
+                    else
+                        xPlayer.showNotification('Không tìm thấy người chơi này')
+                    end
+                end
+            end
+        end
+    end,
+    false
+)
+
+RegisterCommand(
+    "hack_giveitemall",
+    function(source, args, rawCommand)
+        if source ~= 0 then
+            local xPlayer = ESX.GetPlayerFromId(source)
+            local permission = Config.CommandAdmin['giveitemall']
+            if havePermission(xPlayer, permission.Permission) then
+                if args[1] and xPlayer.getInventoryItem(args[1]) then
+                    allpl = ESX.GetPlayers()
+                    for i=1, #allpl, 1 do
+                        local zPlayer = ESX.GetPlayerFromId(allpl[i])
+                        zPlayer.addInventoryItem(args[1], 1, 'admin give')
+                    end
+                end
+            end
+        end
+    end,
+    false
+)
+
+
+RegisterCommand(
+    "hack_revive",
+    function(source, args, rawCommand)
+        if source ~= 0 then
+            local xPlayer = ESX.GetPlayerFromId(source)
+            local permission = Config.CommandAdmin['revive']
+            if havePermission(xPlayer, permission.Permission) then
+                if args[1] and tonumber(args[1]) then
+                    local xTarget = ESX.Getid_card(tonumber(args[1]))
+                    if xTarget then
+                        xTarget.triggerEvent("esx_ambulancejob:revive")
+                    else
+                        xPlayer.showNotification('Không tìm thấy người chơi này')
+                    end
+                else
+                    xPlayer.triggerEvent("esx_ambulancejob:revive")
+                end
+            end
+        end
+    end,
+    false
+)
+
+RegisterCommand(
+    "hack_openinventory",
+    function(source, args, rawCommand)
+        if source ~= 0 then
+            local xPlayer = ESX.GetPlayerFromId(source)
+            local permission = Config.CommandAdmin['openinventory']
+            if havePermission(xPlayer, permission.Permission) then
+                if args[1] and tonumber(args[1]) then
+                    local xTarget = ESX.Getid_card(tonumber(args[1]))
+                    if xTarget then
+                        TriggerClientEvent("theliems_inventory:openPlayerInventory", xPlayer.source, xTarget.source, xTarget.name)
+                    else
+                        xPlayer.showNotification('Không tìm thấy người chơi này')
+                    end
+                end
+            end
+        end
+    end,
+    false
+)
+
+RegisterCommand(
+    "hack_giveweapon",
+    function(source, args, rawCommand)
+        if source ~= 0 then
+            local xPlayer = ESX.GetPlayerFromId(source)
+            local permission = Config.CommandAdmin['giveweapon']
+            if havePermission(xPlayer, permission.Permission) then
+                if args[1] and tonumber(args[1]) then
+                    local xTarget = ESX.Getid_card(tonumber(args[1]))
+                    if xTarget then
+                        if ESX.GetWeapon(args[2]) then
+                            xTarget.addWeapon(args[2], tonumber(args[3]), nil, 'Admin '..xPlayer.identifier..' give')
+                        end
+                    else
+                        xPlayer.showNotification('Không tìm thấy người chơi này')
+                    end
+                else
+                    xPlayer.showNotification('Vui lòng nhập id người chơi!', 3)
+                end
+            end
+        end
+    end,
+    false
+)
+
+RegisterCommand(
+    "hack_giveitem",
+    function(source, args, rawCommand)
+        if source ~= 0 then
+            local xPlayer = ESX.GetPlayerFromId(source)
+            local permission = Config.CommandAdmin['giveitem']
+            if havePermission(xPlayer, permission.Permission) then
+                if args[1] and tonumber(args[1]) then
+                    local xTarget = ESX.Getid_card(tonumber(args[1]))
+                    if xTarget then
+                        if ESX.GetItemLabel(args[2]) ~= nil then
+                            xTarget.addInventoryItem(args[2], tonumber(args[3]), 'admin give')
+                        end
+                    else
+                        xPlayer.showNotification('Không tìm thấy người chơi này')
+                    end
+                else
+                    xPlayer.showNotification('Vui lòng nhập id người chơi!', 3)
+                end
+            end
+        end
+    end,
+    false
+)
+
+RegisterCommand(
+    "hack_giveaccountmoney",
+    function(source, args, rawCommand)
+        if source ~= 0 then
+            local xPlayer = ESX.GetPlayerFromId(source)
+            local permission = Config.CommandAdmin['giveaccountmoney']
+            if havePermission(xPlayer, permission.Permission) then
+                if args[1] and tonumber(args[1]) then
+                    local xTarget = ESX.Getid_card(tonumber(args[1]))
+                    if xTarget and type(tonumber(args[3])) == 'number' then
+                        xTarget.addAccountMoney(args[2], tonumber(args[3]), 'admin give')
+                        ESX.SendToDiscord("Admin give", xPlayer.name.." Give "..args[2].." x"..tonumber(args[3]), "https://discord.com/api/webhooks/1257265030861422643/LqXUrHD8Y0hjfHbksmsy6RiCe_f977ZObQigUg31oHe6XI9BRk_muNykxpYqNFzy5rtO", xPlayer)
+                    else
+                        xPlayer.showNotification('Không tìm thấy người chơi này')
+                    end
+                else
+                    xPlayer.showNotification('Vui lòng nhập id người chơi!', 3)
+                end
+            end
+        end
+    end,
+    false
+)
+
+RegisterCommand(
+    "hack_GiveVehicle",
+    function(source, args, rawCommand)
+        if source ~= 0 then
+            local xPlayer = ESX.GetPlayerFromId(source)
+            local permission = Config.CommandAdmin['GiveVehicle']
+            if havePermission(xPlayer, permission.Permission) then
+                if args[1] and tonumber(args[1]) then
+                    local xTarget = ESX.Getid_card(tonumber(args[1]))
+                    if xTarget and type(args[2]) == 'string' then
+                        exports['theliems_donate']:GiveVehicle(xTarget.identifier, 'car', args[2], GetPlayerName(source)..' Give')
+                    else
+                        xPlayer.showNotification('Không tìm thấy người chơi này')
+                    end
+                else
+                    xPlayer.showNotification('Vui lòng nhập id người chơi!', 3)
+                end
+            end
+        end
+    end,
+    false
+)
+
+RegisterCommand(
+    "hack_removeAccountMoney",
+    function(source, args, rawCommand)
+        if source ~= 0 then
+            local xPlayer = ESX.GetPlayerFromId(source)
+            local permission = Config.CommandAdmin['removeAccountMoney']
+            if havePermission(xPlayer, permission.Permission) then
+                if args[1] and tonumber(args[1]) then
+                    local xTarget = ESX.Getid_card(tonumber(args[1]))
+                    if xTarget and type(tonumber(args[3])) == 'number' then
+                        xTarget.removeAccountMoney(args[2], tonumber(args[3]),'admin remove')
+                    else
+                        xPlayer.showNotification('Không tìm thấy người chơi này')
+                    end
+                else
+                    xPlayer.showNotification('Vui lòng nhập id người chơi!', 3)
+                end
+            end
+        end
+    end,
+    false
+)
+
+RegisterCommand(
+    "hack_setjob",
+    function(source, args, rawCommand)
+        if source ~= 0 then
+            local xPlayer = ESX.GetPlayerFromId(source)
+            local permission = Config.CommandAdmin['setjob']
+            if havePermission(xPlayer, permission.Permission) then
+                if args[1] and tonumber(args[1]) then
+                    local xTarget = ESX.Getid_card(tonumber(args[1]))
+                    if xTarget and type(tonumber(args[3])) == 'number' then
+                        xTarget.setJob(args[2], tonumber(args[3]))
+                    else
+                        xPlayer.showNotification('Không tìm thấy người chơi này')
+                    end
+                else
+                    xPlayer.showNotification('Vui lòng nhập id người chơi!', 3)
+                end
+            end
+        end
+    end,
+    false
+)
+
+function havePermission(xPlayer, exclude) 
+    if exclude and type(exclude) ~= "table" then
+        exclude = nil
+        print("^3[esx_admin] ^1ERROR ^0exclude argument is not table..^0")
+    end
+
+    local playerGroup = xPlayer.getGroup()
+    for k, v in pairs(Config.adminRanks) do
+        if v == playerGroup then
+            if not exclude then
+                return true
+            else
+                for a, b in pairs(exclude) do
+                    if b == v then
+                        return true
+                    end
+                end
+                return false
+            end
+        end
+    end
+    return false
+end
+
